df9932dfe3dafa455d95ef1a7c3a71a7f270b5a6
Christian Wentling <christianwentling@gmail.com>
2017-06-26 15:42:27 -0400

    converts to 180 degree scale

--- PublicArt/app/Camera.js
+++ PublicArt/app/Camera.js
@@ -8,45 +8,97 @@ export default class AppCamera extends Component {
         super(props);
     }
 
-    getDistanceInMeters(x1, x2, y1, y2) {
+    getDistanceInMeters(long1, long2, lat1, lat2) {
         // haversine
-        let R = 6378.137 // radius of eath in KM
-        let dLat = x2 * Math.PI / 180 - x1 * Math.PI / 180;
-        let dLng = y2 * Math.PI / 180 - y1 * Math.PI / 180;
-        let a = Math.sin(dLat/2) * Math.sin(dLng/2) + Math.cos(x1 * Math.PI / 180) * Math.cos(x2 * Math.PI / 180)
-                * Math.sin(dLng/2) * Math.sin(dLng/2);
-        let c = 2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        let R = 6371 // radius of eath in KM
+        let dLat = (lat2 - lat1) * (Math.PI / 180);
+        let dLng = (long2 - long1) * (Math.PI / 180);
+        let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180)
+                * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+        let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         let d = R * c;
         return d * 1000; // Meters
     }
 
     // this returns the relative direction of the POI - for example, if user is facing south
     // but the attraction is due north, then this will return 180
-    getDirection(x1, x2, y1, y2, userDir) {
-        // converts the differences btwn lat's and lng's into radians
-        let dLat = (x2-x1) * (Math.PI / 180);
-        let dLng = (y2-y1) * (Math.PI / 180);
-        // convert current lcoation lat and lng to radians
-        x1 = x1 * (Math.PI / 180);
-        y1 = y1 * (Math.PI / 180);
-        let y = Math.sin(dLng) * Math.cos(y2);
-        let x = Math.cos(x1) * Math.sin(x2) - Math.sin(x1) * Math.cos(x2) * Math.cos(dLng)
-        let brng = Math.atan2(y, x) * (180 / Math.PI)
-        if (brng < 0) brng = 360-Math.abs(brng);
-        return brng - userDir;
+    getDirection(long1, long2, lat1, lat2, userDir) {
+        // // converts the differences btwn lat's and lng's into radians
+        // let dLat = (lat2 - lat1) * (Math.PI / 180);
+        // let dLng = (long2 - long1) * (Math.PI / 180);
+        // // convert current lcoation lat and lng to radians
+        // long1 = long1 * (Math.PI / 180);
+        // lat1 = lat1 * (Math.PI / 180);
+        // let y = Math.sin(dLng) * Math.cos(lat2);
+        // let x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLng)
+        // let brng = Math.atan2(y, x) * (180 / Math.PI)
+        // // if (brng < 0) brng = 360 - Math.abs(brng);
+        // // return brng - userDir;
+        // return brng;
+
+        let theta = Math.atan2(lat2 - lat1, long2 - long1);
+        if (theta < 0){
+          theta += (2 * (Math.PI))
+        }
+        theta =  theta * (180 / Math.PI);
+        let relDir;
+
+        ///VERY DUMB MATH//
+
+        function converter(userDir, theta){
+   const absDiff = Math.abs(theta - userDir); //is abs diff > 180?
+   relDir = absDiff;
+
+   let left;
+   let otherWayAround;
+   if (absDiff > 180) { //is the difference greater than 180?
+     otherWayAround = true;
+   }
+   else {
+     otherWayAround = false;
+   }
+
+   if ((userDir > theta) && (!otherWayAround) || (userDir < theta) && (otherWayAround)) { //is the object to the left or the right?
+     left = true;
+   }
+   else {
+     left = false;
+   }
+
+   if (otherWayAround) {
+     if (left){
+       relDir = 360 - theta + userDir;
+     }
+     else {
+       relDir = 360 - userDir + theta;
+     }
+   }
+
+   if (left){
+     relDir = -relDir;
+   }
+
+    return relDir;
+  }
+
+        console.log('USERDIR==>', userDir);
+        console.log('THETA==>', theta);
+        console.log('RELDIR==>', relDir);
+
+        return relDir;
     }
 
     render() {
         /* gets all pois and their lats and lngs */
-        let relPosition =[];
+        let relPosition =  [];
         this.props.pois.forEach(poi => {
-            let x1 = +poi.lat;
-            let y1 = +poi.lng;
-            let x2 = +this.props.position.coords.latitude;
-            let y2 = +this.props.position.coords.longitude;
+            let lat2 = +poi.lat;
+            let long2 = +poi.lng;
+            let lat1 = +this.props.position.coords.latitude;
+            let long1 = +this.props.position.coords.longitude;
             let relativePos = {
-              distance: this.getDistanceInMeters(x1, x2, y1, y2),
-              dir: this.getDirection(x1, x2, y1, y2, this.props.heading)
+              distance: this.getDistanceInMeters(long1, long2, lat1, lat2),
+              dir: this.getDirection(long1, long2, lat1, lat2, this.props.heading)
             }
             relPosition.push(relativePos)
           })
@@ -60,7 +112,7 @@ export default class AppCamera extends Component {
             >
                 <Text>Heading: {this.props.heading}</Text>
                 {
-                    (relPosition.length) ? <Text> First POI distance: {relPosition[0].distance} and direction: {relPosition[0].dir} </Text>
+                    (relPosition.length) ? <Text> First POI distance: {relPosition[0].distance} and direction: {relPosition[0].dir} and name: {this.props.pois[0].name} </Text>
                     : null
                 }
                 <Text>Hello this is on a cam</Text>
