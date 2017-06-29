ed861ed5cd48c5dda5d1e497472ce972933de9a3
Christian Wentling <christianwentling@gmail.com>
2017-06-29 12:51:24 -0400

    removing console logs and comments

--- PublicArt/app/Mapview.js
+++ PublicArt/app/Mapview.js
@@ -12,7 +12,6 @@ import MapCallout from './MapCallout';
 class Mapview extends Component {
   constructor(props){
     super(props)
-    // console.log(this.props);
     var userLocation = this.props.navigation.state.params.userLocation.coords
     this.state = {
       region: {
@@ -24,7 +23,6 @@ class Mapview extends Component {
       markers: this.props.navigation.state.params.markers,
       selectedMarker: false
     };
-    // this.selectMarker = this.selectMarker.bind(this);
   }
 
 
@@ -67,65 +65,6 @@ class Mapview extends Component {
     );
   }
   }
-// selectMarker(){
-//   console.log('PRESSED')
-//   console.log('BEFORESTATAE', this.state)
-//   this.setState({
-//     selectedMarker: true
-//   });
-//   console.log('STATE=>', this.state);
-// }
-// render() {
-//   const navigation = this.props.navigation
-//   return (
-//     <View style={styles.container}>
-//     <MapView
-//     style={ styles.map }
-//       initialRegion={this.state.region}>
-//         <MapView.Marker
-//           coordinate={{
-//             latitude: this.state.region.latitude,
-//             longitude: this.state.region.longitude
-//           }}
-//           image={require('../PersonIcon.png')}
-//         />
-//         {this.state.markers.map((marker, index) => (
-//           marker.lat > 0 ?
-//              <MapView.Marker
-//                 coordinate={{
-//                   latitude: +marker.lat,
-//                   longitude: +marker.lng}}
-//                   onSelect={this.selectMarker}>
-//                   {this.state.selectedMarker ?
-//                   <MapView.Callout tooltip style={styles.customView}>
-//                     <MapCallout>
-//                       <Text>{marker.name}</Text>
-//                       <Image
-//                         source={{uri: `https://www.nycgovparks.org${marker.thumb_path}`}}
-//                         style={{height: 75, width: 75}}
-//                           />
-//                         <Text></Text>
-//                       <Button
-//                           style={{color: 'blue'}}
-//                           title="Learn More"
-//                           onPress={() => navigation.navigate('Details', { name: marker.link})}
-//                           />
-//                     </MapCallout>
-//                   </MapView.Callout>
-//                   : null}
-//             </MapView.Marker>
-//             : null
-//         ))}
-//       </MapView>
-//     </View>
-//   );
-// }
-// }
-
-// title={marker.name}
-// imageSourcePath={marker.thumb_path}
-// detailsLink={marker.link}
-// navigation = {navigation}>
 
 const styles = StyleSheet.create({
   customView: {
