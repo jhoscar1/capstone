9c2ce75d85de14b68873b53ea424fa50d529ba9b
Christian Wentling <christianwentling@gmail.com>
2017-06-30 15:31:41 -0400

    refactors utils function

--- PublicArt/app/MapCallout.js
+++ PublicArt/app/MapCallout.js
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
     borderColor: '#000000',
     borderWidth: 0.5,
     zIndex: 2,
-    position: 'relative'
+    position: 'absolute'
   },
   amount: {
     flex: 1,
