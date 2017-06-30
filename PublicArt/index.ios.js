de3ec4a422b6b3830a412780dfbf3c5dc5936601
Danielle Westerman <daniellepwesterman@gmail.com>
2017-06-30 16:04:43 -0400

    cosmetic fixes for index.ios.js

--- PublicArt/index.ios.js
+++ PublicArt/index.ios.js
@@ -6,13 +6,13 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  DeviceEventEmitter,
-  TouchableWithoutFeedback
+ 	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	Dimensions,
+	DeviceEventEmitter,
+	TouchableWithoutFeedback
 } from 'react-native';
 import {StackNavigator} from 'react-navigation';
 import { Accelerometer, Gyroscope } from 'react-native-sensors';
@@ -24,166 +24,158 @@ import firebaseApp from './firebase';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class PublicArt extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      headingIsSupported: false,
-      heading: '',
-      position: '',
-      nearbyPois: [],
-      allPois: [],
-      gyro: {}
-    }
-    this.onMapPress = this.onMapPress.bind(this)
+	constructor(props) {
+		super(props);
+		this.state = {
+			headingIsSupported: false,
+			heading: '',
+			position: '',
+			nearbyPois: [],
+			allPois: [],
+			gyro: {}
+		}
+		this.onMapPress = this.onMapPress.bind(this)
+	}
+
+	static navigationOptions = (props) => {
+		var navigation = props.navigation
+		return {
+			headerRight: <Icon name="ios-map" size={30} iconStyle={marginLeft=20}
+							onPress={navigation.state.params && navigation.state.params.handleMapInfo}>
+							<Text>  </Text>
+						 </Icon>,
+			title: 'I (AR)t NY'
+		};
+	}
+
+	componentDidMount() {
+		const accelerationObservable = new Accelerometer({
+			updateInterval: 100,
+		});
+
+
+		accelerationObservable
+		.subscribe(speed => this.setState({gyro: speed}));
+		/* get direction of user */
+		ReactNativeHeading.start(15)
+		.then(didStart => {
+			this.setState({'headingIsSupported': didStart})
+		})
+
+		DeviceEventEmitter.addListener('headingUpdated', data => {
+			this.setState({'heading': data.heading})
+		})
+
+		let nearbyPOIs = [];
+		/* get location of current user*/
+		navigator.geolocation.getCurrentPosition((position) => {
+			this.setState({'position': position});
+			firebaseApp.database().ref('/').orderByChild('name')
+			.on('value', snapshot => {
+				let allpois = snapshot.val();
+
+				if (typeof allpois == 'object') {
+					allpois = Object.values(allpois);
+				}
+
+				this.setState({allPois: allpois});
+				allpois.forEach(poi => {
+					let x1 = +poi.lat;
+					let y1 = +poi.lng;
+					let x2 = +this.state.position.coords.latitude;
+					let y2 = +this.state.position.coords.longitude;
+					let distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2))
+					if (distance < 0.008) nearbyPOIs.push(poi);
+				})
+			},
+			(error) => console.error(error),
+			{timeout: 25000, enableHighAccuracy: true, maximumAge: 1000})
+		});
+
+		this.watchID = navigator.geolocation.watchPosition((newPosition) => {
+			this.setState({'position': newPosition });
+			nearbyPOIs = [];
+			this.setState({'nearbyPois': []})
+			this.state.allPois.forEach(poi => {
+				let x1 = +poi.lat;
+				let y1 = +poi.lng;
+				let x2 = +this.state.position.coords.latitude;
+				let y2 = +this.state.position.coords.longitude;
+				let distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2))
+				if (distance < 0.008) nearbyPOIs.push(poi);
+			})
+			this.setState({'nearbyPois': nearbyPOIs})
+		},
+		(error) => console.error(error),
+		{timeout: 10000, enableHighAccuracy: true, maximumAge: 1000, distanceFilter: 3})
+		this.props.navigation.setParams({ handleMapInfo: this.onMapPress });
+	}
+
+	componentWillUnmount() {
+		navigator.geolocation.clearWatch(this.watchID);
+	}
+
+	onMapPress(){
+		this.props.navigation.navigate('Mapview', {userLocation: this.state.position, markers: this.state.allPois})
+	}
+
+
+	render() {
+		const { navigation } = this.props;
+		return (<View style={styles.container}>
+					<AppCamera tilt={this.state.gyro} pois={this.state.nearbyPois} position={this.state.position}
+							heading={this.state.heading} navigation={navigation} />
+	  			</View>);
+	}
 }
 
-  componentDidMount() {
-    const accelerationObservable = new Accelerometer({
-      updateInterval: 100, // defaults to 100ms
-    });
 
-  // Normal RxJS functions
-  accelerationObservable
-  .subscribe(speed => this.setState({gyro: speed}));
-    /* get direction of user */
-    ReactNativeHeading.start(15)
-    .then(didStart => {
-      this.setState({'headingIsSupported': didStart})
-    })
-
-    DeviceEventEmitter.addListener('headingUpdated', data => {
-      this.setState({'heading': data.heading})
-    })
-
-    let nearbyPOIs = [];
-    /* get location of current user*/
-    navigator.geolocation.getCurrentPosition((position) => {
-        this.setState({'position': position});
-        firebaseApp.database().ref('/').orderByChild('name')
-        .on('value', snapshot => {
-          let allpois = snapshot.val();
-          console.log(typeof allpois);
-          if (typeof allpois == 'object') {
-            allpois = Object.values(allpois);
-          }
-          console.log(allpois)
-
-          this.setState({allPois: allpois});
-          allpois.forEach(poi => {
-            let x1 = +poi.lat;
-            let y1 = +poi.lng;
-            let x2 = +this.state.position.coords.latitude;
-            let y2 = +this.state.position.coords.longitude;
-            let distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2))
-            if (distance < 0.008) nearbyPOIs.push(poi);
-        })
-      },
-      (error) => console.error(error),
-      {timeout: 25000, enableHighAccuracy: true, maximumAge: 1000}
-    )
-  });
-
-    this.watchID = navigator.geolocation.watchPosition((newPosition) => {
-        this.setState({'position': newPosition });
-        nearbyPOIs = [];
-        this.state.allPois.forEach(poi => {
-            let x1 = +poi.lat;
-            let y1 = +poi.lng;
-            let x2 = +this.state.position.coords.latitude;
-            let y2 = +this.state.position.coords.longitude;
-            let distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2))
-            if (distance < 0.008) nearbyPOIs.push(poi);
-        })
-        this.setState({'nearbyPois': nearbyPOIs})
-      },
-      (error) => console.error(error),
-      {timeout: 10000, enableHighAccuracy: true, maximumAge: 1000, distanceFilter: 3}
-    )
-    this.props.navigation.setParams({ handleMapInfo: this.onMapPress });
-  }
-
-  componentWillUnmount() {
-    navigator.geolocation.clearWatch(this.watchID);
-  }
-
-  onMapPress(){
-    this.props.navigation.navigate('Mapview', {userLocation: this.state.position, markers: this.state.allPois})
-  }
-
-  static navigationOptions = (props) => {
-      console.log('PROPS',props);
-      var navigation = props.navigation
-      return {
-        headerRight:
-          <Icon
-            name="ios-map"
-            size={30}
-            iconStyle={marginLeft=20}
-            onPress={navigation.state.params && navigation.state.params.handleMapInfo}>
-          <Text>  </Text></Icon>,
-        title: 'I (AR)t NY'
-        };
-    }
-
-  render() {
-    const { navigation } = this.props;
-    return (
-      <View style={styles.container}>
-        <AppCamera tilt={this.state.gyro} pois={this.state.nearbyPois} position={this.state.position} heading={this.state.heading} navigation={navigation} />
-      </View>
-    );
-  }
-
-
-
-}
 
 // App Router
-
 const AppRouter = StackNavigator({
-    Home: {
-        screen: PublicArt
-    },
-    Details: {
-        screen: PointDetails,
-        path: 'poi/:name'
-    },
-    Mapview: {
-      screen: Mapview
-    }
+	Home: {
+	screen: PublicArt
+	},
+	Details: {
+	screen: PointDetails,
+	path: 'poi/:name'
+	},
+	Mapview: {
+	  screen: Mapview
+	}
 })
 
-// Stylesheet
 
+// Stylesheet
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+	flex: 1,
+	justifyContent: 'center',
+	alignItems: 'center',
   },
   button: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(52, 52, 52, 1.0)',
-    borderRadius: 10
+	flex: 1,
+	justifyContent: 'center',
+	alignItems: 'center',
+	backgroundColor: 'rgba(52, 52, 52, 1.0)',
+	borderRadius: 10
   },
   welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+	fontSize: 20,
+	textAlign: 'center',
+	margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+	textAlign: 'center',
+	color: '#333333',
+	marginBottom: 5,
   },
   preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    height: Dimensions.get('window').height,
-    width: Dimensions.get('window').width
+	flex: 1,
+	justifyContent: 'flex-end',
+	alignItems: 'center',
+	height: Dimensions.get('window').height,
+	width: Dimensions.get('window').width
   }
 });
 
